fb5d6add6d917be09faeeb7022fe7301cee3b7b4
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2020-07-03 01:26:07 +0900

    Change format

--- plugins/arxivbot.py
+++ plugins/arxivbot.py
@@ -7,23 +7,32 @@ FIELDS = ['id', 'title', 'authors', 'date_tags_comment', 'summary']
 
 pp = PrettyPrinter(indent=4)
 
+
+def generate_message(r):
+    tags = ' | '.join(['{}'.format(t['term']) for t in r['tags']])
+    u = r['updated_parsed']
+    p = r['published_parsed']
+    date = '{:04d}/{:02d}/{:02d}, {:04d}/{:02d}/{:02d}'.format(p.tm_year, p.tm_mon, p.tm_mday, u.tm_year, u.tm_mon, u.tm_mday)
+    comment = r['arxiv_comment'] or ''
+    arxiv_id = re.sub(r'https?://arxiv.org/abs/([0-9v\.]+)', r'\1', r['id'])
+    arxiv_id_no_v = re.sub(r'v[0-9]+$', r'', arxiv_id)
+    vanity = '<https://www.arxiv-vanity.com/papers/{}/|vanity>'.format(arxiv_id_no_v)
+    img = '<http://www.arxiv-sanity.com/static/thumbs/{}.pdf.jpg|img>'.format(arxiv_id)
+    lines = [
+        # r['id'],
+        re.sub(r'\n', r' ', r['title']),
+        ', '.join(r['authors']),
+        ', '.join([date, vanity, img, tags, comment]),
+        re.sub(r'\n', r' ', r['summary']),
+    ]
+    return '\n'.join(lines)
+
 @listen_to('https?://arxiv.org/abs/([0-9v\.]+)')
 def arxiv_abs(message, arxiv_id):
-    results = query(id_list=[arxiv_id])
-    pp.pprint(results)
-    if results and len(results) > 0:
-        r = results[0]
-        # r['id'] = re.sub('^http://', 'https://', r['id'])
-        r['id'] = 'https://arxiv.org/abs/' + arxiv_id
-        r['title'] = re.sub('\n', ' ', r['title'])
-        r['authors'] = ', '.join(r['authors'])
-        tags = ' | '.join([t['term'] for t in r['tags']])
-        u = r['updated_parsed']
-        p = r['published_parsed']
-        date = '{}/{}/{}, {}/{}/{}'.format(p.tm_year, p.tm_mon, p.tm_mday, u.tm_year, u.tm_mon, u.tm_mday)
-        comment = r['arxiv_comment'] or ''
-        r['date_tags_comment'] = date + ', ' + tags + ', ' + comment
-        r['summary'] = re.sub('\n', ' ', r['summary'])
-        message.send('\n'.join([str(r[field]) for field in FIELDS]))
+    rs = query(id_list=[arxiv_id])
+    # pp.pprint(rs)
+    if rs and len(rs) > 0:
+        # message.send(generate_message(rs[0]))
+        message.reply_webapi(generate_message(rs[0]), in_thread=True)
     else:
         message.send('No results found: ' + arxiv_id)
